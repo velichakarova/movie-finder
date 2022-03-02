6e0471d6309f2338e57da129d2b98313f59f7247
Velina Chakarova <65416281+velichakarova@users.noreply.github.com>
2022-03-02 18:22:00 +0000

    initial

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatIconModule } from "@angular/material/icon";
+import { MatBadgeModule} from "@angular/material/badge";
+import { MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
+
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatSliderModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatListModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatExpansionModule,
+  MatCardModule,
+  MatTabsModule,
+  MatStepperModule,
+  MatInputModule,
+  MatSelectModule,
+  MatAutocompleteModule,
+]
+
+@NgModule({
+  
+  imports: [ MaterialComponents ],
+  exports:[ MaterialComponents]
+})
+export class MaterialModule { }
